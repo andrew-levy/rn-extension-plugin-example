32af16816d165b498798de52610573a624ef3c39
andrew-levy <alevy97@gmail.com>
2022-07-08 15:12:56 -0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,12 @@
+# Safari Extension Config Plugin Example
+
+Example Expo project using the [Safari Extension Config Plugin](https://github.com/andrew-levy/react-native-safari-extension#readme). Leverages HMR and `react-native-web` to help build Safari extensions.
+
+## Setup
+
+1. Clone the repo
+2. Run `yarn` to install dependencies
+3. Run `yarn ios` to build the extension to your simulator
+4. Run `yarn extension` to enable HMR (in a separate terminal)
+5. Open your extension in Safari and enable it.
+6. Edit `App.tsx` to see changes in the extension
